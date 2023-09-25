f6d4295b76866d88a284d696c61a86424c4e093b
fredriknystrom <fredriknystroms@gmail.com>
2023-09-25 14:37:44 +0200

    added data to the mysql database

--- spotify_project/spotify_project/settings.py
+++ spotify_project/spotify_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'spotify_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spotify_2023',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',         # MySQL default port
     }
 }
 
--- spotify_project/spotifyapp/models.py
+++ spotify_project/spotifyapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+PERCENTAGE_CHOICES = (
+    (1, 'Low'),
+    (2, 'Medium'),
+    (3, 'High')
+)
+
+class SpotifyStats(models.Model):
+    title = models.CharField(max_length=255)
+    artists = models.ManyToManyField(Artist)
+    released_year = models.PositiveIntegerField()
+    released_month = models.PositiveIntegerField()
+    released_day = models.PositiveIntegerField()
+    in_spotify_playlists = models.PositiveIntegerField()
+    in_spotify_charts = models.PositiveIntegerField()
+    streams = models.PositiveIntegerField()
+    bpm = models.PositiveIntegerField()
+    danceability_percentage = models.PositiveSmallIntegerField(choices=PERCENTAGE_CHOICES)
+    energy_percentage = models.PositiveSmallIntegerField(choices=PERCENTAGE_CHOICES)
+    acousticness_percentage = models.PositiveSmallIntegerField(choices=PERCENTAGE_CHOICES)
+    speechiness_percentage = models.PositiveSmallIntegerField(choices=PERCENTAGE_CHOICES)
+
+    def __str__(self):
+        return f"{self.title}, {self.artists}"
